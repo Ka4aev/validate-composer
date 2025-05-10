b2f2690763bd5e934f69cde3b8d4256e774956fd
Ka4aev <staskachaev205@mail.ru>
2025-05-10 22:29:26 +0700

    add new validators

--- src/Validators/PasswordValidator.php
+++ src/Validators/PasswordValidator.php
@@ -1,1 +1,12 @@
 <?php
+namespace Collect\Validators;
+
+class PasswordValidator extends AbstractValidator
+{
+    protected string $message = 'Пароль должен содержать минимум 6 символов, включая цифру, заглавную букву и спецсимвол';
+
+    public function rule(): bool
+    {
+        return preg_match('/^(?=.*[A-Z])(?=.*\d)(?=.*[^\w\d\s]).{6,}$/', $this->value);
+    }
+}
--- src/Validators/RequireValidator.php
+++ src/Validators/RequireValidator.php
@@ -1,1 +1,11 @@
 <?php
+namespace Collect\Validators;
+
+class RequireValidator extends AbstractValidator
+{
+    protected string $message = 'Поле :field обязательно для заполнения';
+    public function rule(): bool
+    {
+        return !empty($this->value);
+    }
+}
